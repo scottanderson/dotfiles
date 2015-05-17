3a0e2af14a0691a46e95f3dd194226ff764dc0c1
Scott Anderson <scotta@gmail.com>
2015-05-16 21:55:42 -0400

    git config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git submodule update --init
+git config --global alias.l "log --graph --abbrev-commit --decorate --date=relative --format='%C(auto)%h - %C(bold green)(%ar)%C(reset)%C(auto) %s - %C(bold)%an%C(reset)%C(auto)%d'"
 git config --global core.excludesfile '~/.cvsignore'
+git config --global url.ssh://git@github.com/.pushinsteadof git://github.com/
+git config --global url.ssh://git@github.com/.pushinsteadof https://github.com/
 for f in cvsignore screenrc vim vimrc; do
     source="$HOME/.$f"
     target="dotfiles/$f"
