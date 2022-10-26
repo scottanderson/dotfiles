b2ac8867c9b91c213893bbbc69abf129a4276583
Scott Anderson <scotta@gmail.com>
2022-10-25 23:05:41 -0400

    git lc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 git submodule update --init
 git config --global alias.l "log --graph --abbrev-commit --decorate --date=relative --format='%C(auto)%h - %C(bold green)(%ar)%C(reset)%C(auto) %s - %C(bold)%an%C(reset)%C(auto)%d'"
+git config --global alias.lc '!f() {
+    : git log
+    if [ $# -eq 0 ]; then
+        f HEAD @{upstream};
+    elif [ $# -eq 1 ]; then
+        f HEAD $1;
+    else
+        git l "$@" ^$(git merge-base -a --octopus "$@")^@;
+    fi;
+}; f'
 git config --global core.excludesfile '~/.cvsignore'
 git config --global url.ssh://git@github.com/.pushinsteadof git://github.com/
 git config --global url.ssh://git@github.com/.pushinsteadof https://github.com/
