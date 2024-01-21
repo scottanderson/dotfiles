dd662dc1d2f46885baf772a81d1b12af1b57c203
Scott Anderson <scottanderson@users.noreply.github.com>
2024-01-21 01:48:52 -0500

    edit binary files using xxd

--- config/nvim/lua/startup/autocmd.lua
+++ config/nvim/lua/startup/autocmd.lua
@@ -1,2 +1,14 @@
 -- Move the help split to the right
 vim.cmd [[autocmd FileType help wincmd L]]
+
+vim.cmd [[" vim -b : edit binary using xxd-format!]]
+vim.cmd [[augroup Binary]]
+vim.cmd [[  au!]]
+vim.cmd [[  au BufReadPre   *.bin,*.sav let &bin=1]]
+vim.cmd [[  au BufReadPost  *.bin,*.sav if &bin | %!xxd -g1]]
+vim.cmd [[  au BufReadPost  *.bin,*.sav set ft=xxd | endif]]
+vim.cmd [[  au BufWritePre  *.bin,*.sav if &bin | %!xxd -r]]
+vim.cmd [[  au BufWritePre  *.bin,*.sav endif]]
+vim.cmd [[  au BufWritePost *.bin,*.sav if &bin | %!xxd -g1]]
+vim.cmd [[  au BufWritePost *.bin,*.sav set nomod | endif]]
+vim.cmd [[augroup END]]
