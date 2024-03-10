b5feeaa63044823daddf045be5780032904b2efe
Scott Anderson <scottanderson@users.noreply.github.com>
2024-03-09 22:29:05 -0500

    setup treesitter-context

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -19,3 +19,9 @@ require('nvim-treesitter.configs').setup {
         additional_vim_regex_highlighting = false,
     },
 }
+
+require('treesitter-context').setup {
+    max_lines = 20,       -- How many lines the window should span. Values <= 0 mean no limit.
+    trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    mode = 'topline',     -- Line used to calculate context. Choices: 'cursor', 'topline'
+}
