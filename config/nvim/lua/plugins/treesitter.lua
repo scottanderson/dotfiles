27a57bd3d4d854a180453ca17f8f1c6110d99ebb
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2026-07-13 02:10:09 -0400

    tree-sitter-manager.nvim @ develop

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,17 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'main',
-        lazy = false,
-        build = ":TSUpdate",
+        'romus204/tree-sitter-manager.nvim',
+        branch = 'develop',
         opts = {
-            -- A list of parser names, or 'all'
-            ensure_installed = { 'vimdoc', 'javascript', 'typescript', 'c', 'lua', 'rust' },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
+            ensure_installed = { -- parsers to install at startup
+                'c',
+                'javascript',
+                'lua',
+                'rust',
+                'typescript',
+                'vimdoc',
             },
+            auto_install = true, -- auto-install when a new filetype is encountered
         },
     },
     {
